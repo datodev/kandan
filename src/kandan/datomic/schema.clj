9be5eaacfe80f29aaccec6205fd26966705a4315
Sean Grove <s@bushi.do>
2015-10-18 18:17:29 -0700

    First pass at Kandan schema

--- src/kandan/datomic/schema.clj
+++ src/kandan/datomic/schema.clj
@@ -33,21 +33,82 @@
 (def schema-2
   (vec
    (concat
-    ;; TodoMVC Types
-    [(enum :kandan.types/task)
-     (enum :kandan.types/user)]
+    ;; Kandan Types
+    [(enum :kandan.types/org)
+     (enum :kandan.types/user)
+     (enum :kandan.types/channel)
+     (enum :kandan.types/msg)
+     (enum :kandan.types/file)
+     (enum :kandan.types/notification-setting)]
+
+    ;; Orgs
+    [(attribute :org/name       :db.type/string :db/unique :db.unique/value)
+     (attribute :org/icon       :db.type/uri)
+     (attribute :org/users      :db.type/ref
+                :db/cardinality :db.cardinality/many)
+     (attribute :org/admins     :db.type/ref
+                :db/cardinality :db.cardinality/many)
+     (attribute :org/channels   :db.type/ref
+                :db/cardinality :db.cardinality/many)]
 
     ;; Users
-    [(attribute :user/id          :db.type/uuid :db/unique :db.unique/identity)
+    [(attribute :user/disabled?   :db.type/boolean)
      (attribute :user/email       :db.type/string :db/unique :db.unique/identity)
+     (attribute :user/nick        :db.type/string)
      (attribute :user/given-name  :db.type/string)
-     (attribute :user/family-name :db.type/string)]
+     (attribute :user/family-name :db.type/string)
+     (attribute :user/avatar      :db.type/uri)
+     (attribute :user/notification-settings :db.type/ref
+                :db/cardinality :db.cardinality/many
+                :db/isComponent true)]
     
-    ;; Tasks
-    [(attribute :task/user  :db.type/ref)
-     (attribute :task/title :db.type/string)
-     (attribute :task/completed? :db.type/boolean)
-     (attribute :task/order :db.type/long)])))
+    ;; Channels
+    [(attribute :channel/title   :db.type/string)
+     (attribute :channel/topic   :db.type/string)
+     (attribute :channel/created-at   :db.type/instant)
+     (attribute :channel/admins  :db.type/ref
+                :db/cardinality  :db.cardinality/many)
+     (attribute :channel/members :db.type/ref
+                :db/cardinality  :db.cardinality/many)
+     (attribute :channel/msgs    :db.type/ref
+                :db/cardinality  :db.cardinality/many
+                :db/isComponent  true)
+     (attribute :channel/files   :db.type/ref
+                :db/cardinality  :db.cardinality/many
+                :db/isComponent  true)
+     (attribute :channel/notification-settings :db.type/ref
+                :db/cardinality :db.cardinality/many
+                :db/isComponent true)]
+
+    [(enum :notification.level/all)
+     (enum :notification.level/me)
+     (enum :notification.level/none)]
+
+    ;; Notification setting
+    [(attribute :notification/general           :db.type/ref)
+     (attribute :notification/everyone-and-here :db.type/ref)
+     (attribute :notification/muted?            :db.type/boolean)]
+
+    ;; Messages
+    [(attribute :msg/body       :db.type/string)
+     (attribute :msg/at         :db.type/instant)
+     (attribute :msg/user       :db.type/ref)
+     (attribute :msg/pinned?    :db.type/boolean)
+     (attribute :msg/file       :db.type/ref
+                :db/isComponent true)]
+
+    ;; File
+    [(attribute :file/name               :db.type/string)
+     (attribute :file/comment            :db.type/string)
+     (attribute :file/failed?            :db.type/boolean)
+     (attribute :file/last-modified      :db.type/long)
+     (attribute :file/last-modified-date :db.type/instant)
+     (attribute :file/size               :db.type/long)
+     (attribute :file/type               :db.type/ref)
+     (attribute :file/mime               :db.type/string)
+     (attribute :file/src                :db.type/uri)
+     (attribute :file/progress           :db.type/long)])))
+
 
 (defonce schema-ents
   (atom nil))
@@ -82,3 +143,64 @@
   (ensure-schema))
 
 ;; (init)
+
+(comment
+  ;; Client-side
+  (dato/transact! dato :msg-created
+                  [{:db/id       (d/tempid :db.part/user)
+                    :dato/guid   (d/squuid)
+                    ;; ??? Should msg/at be enforced server-side?
+                    :msg/at      (js/Date.)
+                    :msg/user    {:db/id (:db/id me)}
+                    :msg/channel {:db/id (:db/id channel)}}])
+
+  (dato/transact! dato :msg-created
+                  (let [msg-eid (d/tempid :db.part/user)]
+                    [{:db/id         msg-eid
+                      :dato/guid     (d/squuid)
+                      ;; ??? Should msg/at be enforced server-side?
+                      :msg/at        (js/Date.)
+                      :user/_msgs    [{:db/id msg-eid}]
+                      :channel/_msgs [{:db/id msg-eid}]}])))
+
+(comment
+  ;; msg component
+  (render [_]
+    (let [msg  data
+          user (:user/_msgs msg)]
+      (dom/li {:react-key (:db/id channel)}
+              (dom/div {:class "avatar"}
+                       (dom/img {:src (:user/avatar user)}))
+              (:msg/body msg))))
+
+  ;; channel component
+  (render [_]
+    (let [channel (d/entity db channel-eid)
+          msgs    (:channel/msgs channel)]
+      (dom/div
+       (dom/ul
+        ;; Possible filter here if search is on
+        (for [msg (sort-by :msg/at msgs)]
+          (om/build msg-com msg)))))))
+
+(comment
+  ;; All messages for a user
+  (dsu/qes-by db :msg/_user (:db/id user))
+
+  ;; msg component
+  (render [_]
+    (let [msg  data
+          user (:msg/user msg)]
+      (dom/li {:react-key (:db/id channel)}
+              (dom/div {:class "avatar"}
+                       (dom/img {:src (:user/avatar user)}))
+              (:msg/body msg))))
+
+  ;; channel component
+  (render [_]
+    (let [msgs (dsu/qes-by db :message/channel channel-eid)]
+      (dom/div
+       (dom/ul
+        ;; Possible filter here if search is on
+        (for [msg (sort-by :msg/at msgs)]
+          (om/build msg-com msg)))))))
