96319f0aba9d8450b2e4daa56d5b71691823e682
Sean Grove <s@bushi.do>
2015-11-02 17:23:25 +0100

    Add navigator description to session, with (temporarily) hard-coded git sha

--- src/cljs/kandan/client/config.cljs
+++ src/cljs/kandan/client/config.cljs
@@ -5,3 +5,22 @@
 
 (defn dato-port []
   (aget (config) "dato-port"))
+
+(def navigator-desc
+  (let [navigator (.-navigator js/window)]
+    {:appCodeName         (aget navigator "appCodeName")
+     :appName             (aget navigator "appName")
+     :appVersion          (aget navigator "appVersion")
+     :cookieEnabled       (aget navigator "cookieEnabled")
+     :hardwareConcurrency (aget navigator "hardwareConcurrency")
+     :language            (aget navigator "language")
+     :languages           (js->clj (aget navigator "languages"))
+     :maxTouchPoints      (aget navigator "maxTouchPoints")
+     :onLine              (aget navigator "onLine")
+     :platform            (aget navigator "platform")
+     :product             (aget navigator "product")
+     :productSub          (aget navigator "productSub")
+     :userAgent           (aget navigator "userAgent")
+     :vendor              (aget navigator "vendor")
+     :vendorSub           (aget navigator "vendorSub")
+     :client-git-sha      "8b986b8"}))
